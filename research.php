32bbf9e995d42558a3c4d42c8805bdf09f0a6088
Mahbubur Riad <mahbubur.riad@northsouth.edu>
2022-05-17 20:29:59 +0600

    research updated

--- research.php
+++ research.php
@@ -24,36 +24,41 @@ include('inc/header.php');
                 <div class="row">
                     <div class="col">
                         <div class="lm-info-block gray-default">
-                            <img class="img-fluid research-img" src="img/interest/social-media.png" alt="Social Media & Technology">
+                            <img class="img-fluid research-img" src="img/interest/social-media.png"
+                                 alt="Social Media & Technology">
                             <h4>Social Media & Technology</h4>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="lm-info-block gray-default">
-                            <img class="img-fluid research-img" src="img/interest/conference.png" alt="Political Communication">
-                            <h4>Political Communication</h4>
+                            <img class="img-fluid research-img" src="img/interest/conference.png"
+                                 alt="Political Communication">
+                            <h4>International Political Communication</h4>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="lm-info-block gray-default">
-                            <img class="img-fluid research-img" src="img/interest/management.png" alt="Organizational Communication">
-                            <h4>Organizational Communication</h4>
+                            <img class="img-fluid research-img" src="img/icon/propaganda.png"
+                                 alt="Organizational Communication">
+                            <h4>Disinformation in the Russia-Ukraine War</h4>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="lm-info-block gray-default">
-                            <img class="img-fluid research-img" src="img/interest/politics.png" alt="International Communication">
-                            <h4>International Communication</h4>
+                            <img class="img-fluid research-img" src="img/icon/fake-news.png"
+                                 alt="International Communication">
+                            <h4>Misinformation, Disinformation</h4>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="lm-info-block gray-default">
-                            <img class="img-fluid research-img" src="img/interest/conflict.png" alt="Community Violence">
-                            <h4>Community <br>Violence</h4>
+                            <img class="img-fluid research-img" src="img/icon/protest.png"
+                                 alt="Community Violence">
+                            <h4>Digital Social Movements</h4>
                         </div>
                     </div>
                 </div>
@@ -61,7 +66,28 @@ include('inc/header.php');
 
 
                 <div class="row">
-                    <div class=" col-xs-12 col-sm-6 ">
+                    <div class=" col-xs-12 ">
+
+                        <div class="p-20"></div>
+
+                        <div class="block-title">
+                            <h2>CONFERENCES & POSTER PRESENTATION</h2>
+                        </div>
+
+                        <p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:24.0pt;line-height:107%;font-size:20px;text-indent:-24.0pt;'>
+                            <span style='font-size:16px;line-height:107%;'>Hasan, M. (2022, March 3). A Metaphorical &amp; Visual Analysis of Gender in Al Jazeera &amp; BBC coverage of Afghanistan after the Taliban take over<span
+                                        style="color:#333333;background:white;">&nbsp;</span>[Poster Presentation]. In<em>&nbsp;21<sup>st</sup> Annual Graduate Student Research Poster Competition (pp. 23&ndash;23). Texas Tech University, Lubbock, TX, United States</em></span>
+                        </p>
+
+                        <p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:24.0pt;line-height:107%;font-size:20px;text-indent:-24.0pt;'>
+                            <span style='font-size:16px;line-height:107%;'>Hasan, M. (2021, November 11). In Cradle of a Nation and a Language: The University of Dhaka as a Site of Bangladeshi Collective Memory [Conference Session]. <em>11th Annual Arts &amp; Humanities Conference, Texas Tech University, Lubbock, TX, United States.</em></span>
+                        </p>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class=" col-xs-12 col-sm-6">
 
                         <div class="p-20"></div>
 
@@ -72,15 +98,14 @@ include('inc/header.php');
                         <div class="certificate-item clearfix large-box-item">
                             <div class="certi-content">
 
-                                    <div class="float-left">
-                                        <h5>Texas Tech University</h5>
-                                        <p>Guest Speaker</p>
-                                    </div>
-                                    <div class="float-right text-right">
-                                        <p>Lubbock, TX <br> September 29, 2021</p>
-                                    </div>
-                                    <div class="clearfix"></div>
-
+                                <div class="float-left">
+                                    <h5>Texas Tech University</h5>
+                                    <p>Guest Speaker</p>
+                                </div>
+                                <div class="float-right text-right">
+                                    <p>Lubbock, TX <br> September 29, 2021</p>
+                                </div>
+                                <div class="clearfix"></div>
 
 
                                 <div class="text-center">
@@ -92,7 +117,7 @@ include('inc/header.php');
 
                     </div>
 
-                    <div class=" col-xs-12 col-sm-6 ">
+                    <!--<div class=" col-xs-12 col-sm-6 ">
 
                         <div class="p-20"></div>
 
@@ -103,9 +128,33 @@ include('inc/header.php');
                         <div class="certificate-item large-box-item clearfix">
                             <div class="certi-content">
                                 <div>
-                                    Hasan, M. (2021). In Cradle of a Nation and a Language: The University of Dhaka as a Site of Bangladeshi
+                                    Hasan, M. (2021). In Cradle of a Nation and a Language: The University of Dhaka as a
+                                    Site of Bangladeshi
                                     Collective Memory. <span class="font-italic">11th Annual Arts & Humanities Conference, Texas Tech University.</span>
                                 </div>
+                            </div>
+                        </div>
+                    </div>-->
+
+
+                    <div class="row">
+                        <div class=" col-xs-12 ">
+
+                            <div class="p-20"></div>
+
+                            <div class="block-title">
+                                <h2>AWARDS</h2>
+                            </div>
+                            
+                            <div class="clearfix ">
+                                <ul>
+                                    <li>Graduate Teaching Assistantship, College of Media & Communication, Texas Tech University.</li>
+                                    <li>TTU Helen DeVitt Jones Mass Communication Scholarship Spring 2022.</li>
+                                    <li>TTU Media & Communication Dean’s International Scholarship Spring 2022</li>
+                                    <li>First position, Social Sciences category at the 21st Annual Graduate Student Research Poster Competition 2022, Texas Tech University. </li>
+                                    <li>First Position, Santa Fe Photography Contest 2022, TTU International Affairs.</li>
+                                    <li>Winner, Mobile Category, Leading University Photography Exhibition 2017. </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
